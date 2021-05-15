db30e8f4ebeb1cbb71c6bcb003c6c0668d78e62b
Ripred100 <60947523+Ripred100@users.noreply.github.com>
2021-05-14 23:18:25 -0400

    commented stuff

--- ELO.py
+++ ELO.py
@@ -2,13 +2,16 @@ import json
 import requests
 
 
+#takes python dictionary and writes it to the json file
 def write_json(new_data, filename='players.json'):
     with open(filename, 'w') as file:
         json.dump(new_data, file, indent = 4)
 
 
+#this function takes a name and an ign and adds the player to the players.json file
 def AddPlayer(Name, IGN, filename='players.json'):
 
+    #default ranking values of skill (IQ)=100, and skill confidence (Bungo)=33.3
     Dict = dict([('name', Name), ('ign', IGN), ('IQ', 100.0), ('Bungo', 100.0/3.0)])
     print(Dict)
 
@@ -20,7 +23,9 @@ def AddPlayer(Name, IGN, filename='players.json'):
 
     write_json(data)
 
-def requestMatchData(GameID, APIKey="RGAPI-cbb98062-5c17-4480-92e7-77b8929052c3"):
+#playing with Riot API
+#takes a game ID and returns a bunch of data in the form of a json file
+def requestMatchData(GameID, APIKey="RGAPI-cbb98062-5c17-4480-92e7-77b8929052c3"):#please don't steal my API key :)
     URL = "https://na1.api.riotgames.com/lol/match/v4/matches/" + GameID + "?api_key=" + APIKey
     print(URL)
     response = requests.get(URL)
